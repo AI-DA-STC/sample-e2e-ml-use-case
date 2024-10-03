5649272baf9cf7b050c16376ca3ebdeb7ed3bbc6
Ryzal Kamis <mohamedryzal.binkamis@stengg.com>
2024-10-03 10:57:28 +0800

    Add comments and docstring for mlflow test script.

--- scripts/mlflow_test_script.py
+++ scripts/mlflow_test_script.py
@@ -8,7 +8,14 @@ import mlflow
 
 
 def main():
-    """Main function for testing of logging to MLflow server."""
+    """Main function for testing of logging to MLflow server.
+
+    This function:
+    - logs parameters
+    - logs metrics
+    - logs an artifact
+    - prints the current tracking URI and artifact location
+    """
     mlflow.set_experiment(sys.argv[1])
     dummy_param_1 = 64
     dummy_param_2 = 1.0
@@ -23,15 +30,19 @@ def main():
     mlflow.start_run()
 
     print("Logging parameters...")
+    # Log two dummy parameters
     mlflow.log_param("dummy_param_1", dummy_param_1)
     mlflow.log_param("dummy_param_2", dummy_param_2)
 
     print("Logging metrics...")
+    # Log two dummy metrics for 4 steps
     for step in range(1, 5):
         mlflow.log_metric("dummy_metric_1", time.time(), step=step)
         mlflow.log_metric("dummy_metric_2", random.uniform(0.1, 0.5), step=step)
         time.sleep(2)
 
+    print("Logging artifact...")
+    # Create a text artifact and log it
     dummy_text_content = "This text content should be uploaded to the ECS bucket."
     with open("text_artifact.txt", "w", encoding="utf-8") as file:
         file.write(dummy_text_content)
